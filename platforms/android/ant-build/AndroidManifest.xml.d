8a55c3e84a3c3769aa9c245cc8ea3d69a7cb0bc5
icedocemile <contact@chouetteweb.fr>
2015-01-12 22:27:51 +0100

    Remove the ant-build

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\ant-build\classes.dex \
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\CordovaLib\ant-build\classes.jar \
