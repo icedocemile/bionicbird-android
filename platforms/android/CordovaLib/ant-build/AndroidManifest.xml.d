bc31e4af583522fc3e1e41b92f3842dcb8cae035
icedocemile <contact@chouetteweb.fr>
2015-01-12 22:31:43 +0100

    Remove the android platforms from commit

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Petit Chat\Documents\GitHub\bionicbird-android\platforms\android\CordovaLib\AndroidManifest.xml \
